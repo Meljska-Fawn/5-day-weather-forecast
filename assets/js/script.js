a793bc511901051b60b96ed7cf8a75050e5af33d
Meljska-Fawn <meli.explores@gmail.com>
2023-02-02 23:33:24 -0800

    added html layout and current weather card

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,34 +5,40 @@ let searchButton = document.getElementById('search-button');
 let cityInputEl = document.querySelector('#city-input');
 let currentWeatherEl = document.getElementById('current-weather');
 let headerEl = document.getElementById('fiveday-header');
+let searchHistoryEl = document.getElementById('search-history');
 
 var APIKey = "af6fe5609e13717cb920c672710093bd";
 
 
 function weatherForecast() {
 
-    var cityName = cityInputEl.value.trim();
-    var requestCity = 'http://api.openweathermap.org/geo/1.0/direct?q=' + cityName + '&limit=1&appid=' + APIKey;
+    function getCoordinates() {
+        var cityName = cityInputEl.value.trim();
+        var requestCity = 'http://api.openweathermap.org/geo/1.0/direct?q=' + cityName + '&limit=1&appid=' + APIKey;
 
-    fetch(requestCity)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data)
+        fetch(requestCity)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                console.log(data)
 
-            for (var i = 0; i < data.length; i++) {
-
-                let lat = data[i].lat;
+                let lat = data[0].lat;
                 console.log(lat);
-                let lon = data[i].lon;
+                let lon = data[0].lon;
                 console.log(lon);
 
-                cityCoordinates(lat, lon);
-            }
-        });
+                getWeather(lat, lon);
 
-    function cityCoordinates(lat, lon) {
+            });
+    }
+
+    function getWeather(lat, lon) {
+
+        currentWeatherEl.classList.remove("d-none");
+        headerEl.classList.remove("d-none");
+
+
         var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&limit=5&appid=' + APIKey;
 
         fetch(requestUrl)
@@ -42,38 +48,45 @@ function weatherForecast() {
             .then(function (data) {
                 console.log(data);
 
-                for (var i = 0; i < data.length; i++) {
+                var cityName = document.createElement('h3');
+                var date = document.createElement('h3');
+                var icon = document.createElement('img');
+                var temp = document.createElement('p');
+                var wind = document.createElement('p');
+                var humidity = document.createElement('p');
 
-                    var cityName = document.createElement('h3');
-                    var date = document.createElement('h3');
-                    var icon = document.createElement('p');
-                    var temp = document.createElement('p');
-                    var wind = document.createElement('p');
-                    var humidity = document.createElement('p');
+                cityName.textContent = data.city.name;
 
-                    cityName.textContent = data[i].city.name;
-                    date.textContent = data[i].list.dt_txt;
-                    icon.textContent = data[i].list.weather.icon;
-                    temp.textContent = "Temp:" + data[i].list.main.temp + " &#176F";
-                    wind.textContent = "Wind:" + data[i].list.wind.speed + " mph";
-                    humidity.textContent = "Humidity:" + data[i].list.main.humidity + "%";
+                const currentDay = new Date(data.list[0].dt_txt);
+                const day = currentDay.getDate() - 1;
+                const month = currentDay.getMonth() + 1;
+                const year = currentDay.getFullYear();
+                date.textContent = month + "/" + day + "/" + year;
 
-                    console.log(data[i].city.name);
-                    console.log(icon.textContent);
-                    console.log(temp.textContent);
-                    console.log(wind.textContent);
-                    console.log(humidity.textContent);
+                let iconImg = data.list[0].weather[0].icon;
+                icon.setAttribute("src", "https://openweathermap.org/img/wn/" + iconImg + "@2x.png"); 
+                icon.setAttribute('width', 100);
+                icon.setAttribute('height', 100);
 
-                    currentWeatherEl.append(cityName);
-                    currentWeatherEl.append(date);
-                    currentWeatherEl.append(icon);
-                    currentWeatherEl.append(temp);
-                    currentWeatherEl.append(wind);
-                    currentWeatherEl.append(humidity);
-                }
+                temp.textContent = "Temp: " + convertTemp(data.list[0].main.temp) + " ºF";
+                wind.textContent = "Wind: " + data.list[0].wind.speed + " mph";
+                humidity.textContent = "Humidity: " + data.list[0].main.humidity + "%";
+
+
+                currentWeatherEl.append(cityName);
+                currentWeatherEl.append(date);
+                currentWeatherEl.append(icon);
+                currentWeatherEl.append(temp);
+                currentWeatherEl.append(wind);
+                currentWeatherEl.append(humidity);
             });
     }
 
+    function convertTemp(k) {
+        return Math.floor((k - 273) * 1.8 + 32);
+    }
+
+    searchButton.addEventListener('click', getCoordinates);
 }
 
-searchButton.addEventListener('click', weatherForecast);
+weatherForecast();
